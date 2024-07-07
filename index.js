f789ec82c7d772a28e8d59eded0b68ae2c5f7cba
Daneris Mendoza <106788394+DanerisMendoza@users.noreply.github.com>
2024-07-07 20:38:59 +0800

    allow all origin

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const path = require('path');
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    // origin: 'http://localhost:5173',
+    origin: '*',
     credentials: true // if you are sending cookies or other credentials
 }));
 
